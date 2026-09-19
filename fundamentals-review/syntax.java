495918c21787c1c49f4c3b8d7f115df321881a7b
joshuavlara <65004088+joshuavlara@users.noreply.github.com>
2026-09-20 03:18:02 +1000

    add fundamentals review

---
+++ fundamentals-review/syntax.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(string[] args) {
+    String accountName = "Joshua";
+    double balance = 1500.00;
+    int transaction = 3;
+    boolean active = true;
+
+  }
+}
